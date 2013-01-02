91075d871c6627a236f84baaac9098812f903ba2
Wynn Netherland <wynn@github.com>
2013-01-01 22:30:19 -0600

    intial commit

---
+++ config.rb
@@ -0,0 +1,13 @@
+# Require any additional compass plugins here.
+require 'susy'
+require 'compass-normalize'
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :compressed
